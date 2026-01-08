0b72a38e33611fa9f375f21252e07ec8025b6269
rozhiadadashzadeh-ro <rojhia.dadashzadeh@gmail.com>
2026-01-08 13:05:39 +0330

    Add message when no event is selected

--- src/components/EventDetails.jsx
+++ src/components/EventDetails.jsx
@@ -1,3 +1,10 @@
 export default function EventDetails({ event }) {
+    if (!event) {
+        return (
+            <div className="placeholder">
+                <p>Select an event to view details</p>
+            </div>
+        );
+    }
 
 }
